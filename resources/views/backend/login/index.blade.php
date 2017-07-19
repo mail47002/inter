855af532f3498aac2c433602faf5725ba5ce0f04
Roman K <modern.diz@gmail.com>
2017-07-19 19:41:03 +0300

    Update login

--- resources/views/backend/login/index.blade.php
+++ resources/views/backend/login/index.blade.php
@@ -5,7 +5,6 @@
     <title>Admin Panel - Login</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="{{ asset('packages/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
-    @stack('styles')
     <link href="{{ asset('css/backend/styles.css') }}" rel="stylesheet">
 </head>
 <body>
@@ -42,14 +41,5 @@
             </div>
         </div>
     </div>
-
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-    <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
-    <![endif]-->
-    <script src="{{ asset('js/backend/jquery-2.2.4.min.js') }}"></script>
-    <script src="{{ asset('packages/bootstrap/js/bootstrap.min.js') }}"></script>
-    @stack('scripts')
-    <script src="{{ asset('js/backend/custom.js') }}"></script>
 </body>
 </html>
