006e429dd27945cc4f6f82251579dbb4cdec7452
mail47002 <mail47002@gmail.com>
2017-07-27 02:07:43 +0300

    Banner update

--- app/Banner.php
+++ app/Banner.php
@@ -9,20 +9,4 @@ class Banner extends Model
     protected $fillable = [
         'position_id', 'body', 'status', 'sort_order'
     ];
-
-    // public function setSlugAttribute($value)
-    // {
-    //     $this->attributes['slug'] = $this->makeSlug($value);
-    // }
-
-    // protected function makeSlug($value, $extra = '')
-    // {
-    //     $slug = !empty($extra) ? str_slug($value . '-' . $extra) : str_slug($value);
-
-    //     if ($this->id === null && $this->whereSlug($slug)->exists()) {
-    //         return $this->makeSlug($value, $extra + 1);
-    //     }
-
-    //     return $slug;
-    // }
 }
