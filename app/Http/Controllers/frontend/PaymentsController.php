dc33d29d35dc49a09ee7c16c00be24274d2a0f2c
Roman K <modern.diz@gmail.com>
2017-08-19 16:00:02 +0300

    Update payment

--- app/Http/Controllers/frontend/PaymentsController.php
+++ app/Http/Controllers/frontend/PaymentsController.php
@@ -8,51 +8,48 @@ use App\Payment;
 use App\UserCredit;
 use Auth;
 use App\Services\PaypalPDT;
-use Log;
 
 class PaymentsController extends Controller
 {
-	public function __construct()
-	{
-
-	}
-
 	public function success(Request $request)
 	{
-    $pdt = new PaypalPDT();
+        $pdt = new PaypalPDT();
 
-    $pdt->useSandbox();
+        $pdt->useSandbox();
 
-    $verified = $pdt->verify($request);
+        $verified = $pdt->verify($request);
 
-    if ($verified && Auth::check()) {
-    	// dd($pdt->getPaymentData());
-    	$payment_data = $pdt->getPaymentData();
-    	$entry = Auth::user();
+        if ($verified && Auth::guard('web')->check()) {
+            $userId = Auth::user()->id;
+            $payment_data = $pdt->getPaymentData();
 
-    	$payment = new Payment();
+            // Payment
+            $payment = new Payment();
 
-    	$payment->user_id = $entry->id;
-    	$payment->ammount = $payment_data['mc_gross'];
-    	$payment->currency = $payment_data['mc_currency'];
-    	$payment->paid = $payment_data['txn_id'];;
+            $payment->user_id   = $userId->id;
+            $payment->ammount   = $payment_data['mc_gross'];
+            $payment->currency  = $payment_data['mc_currency'];
+            $payment->paid       = $payment_data['txn_id'];;
 
-    	if ($payment->save()) {
-    		$credits = new UserCredit();
-    		$credits->user_id = $entry->id;
-    		$credits->credits = $payment_data['mc_gross'] * config('settings.one_credits');
-    		$credits->description = 'Pirkti internetu';
-    		$credits->save();
-    	}
-    	return view('frontend.payments.success');
-    }
-	return view('frontend.payments.cancel');
+            $payment->save();
 
+            // User credits
+            $credits = new UserCredit();
+
+            $credits->user_id       = $userId;
+            $credits->credits       = $payment_data['mc_gross'] * config('settings.one_credits');
+            $credits->description   = 'Pirkti internetu';
+
+            $credits->save();
+
+            return view('frontend.payments.success');
+        }
+
+        $this->cancel();
 	}
 
 	public function cancel()
 	{
 		return view('frontend.payments.cancel');
 	}
-
 }
